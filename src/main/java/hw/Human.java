40cd7626c5887e6700165b032cdd947da06e137e
Денис <puteec228@gmail.com>
2024-02-22 17:13:09 +0300

    HW ver 1

---
+++ src/main/java/hw/Human.java
@@ -0,0 +1,17 @@
+package hw;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Human {
+
+    String firstName;
+    String lastName;
+
+
+}
---
+++ src/main/java/hw/Main.java
@@ -0,0 +1,9 @@
+package hw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Human human = new Human("Denis","Haritonuk");
+        System.out.println(human);
+    }
+}
